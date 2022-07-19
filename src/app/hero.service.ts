b65643e4e21f279c6d7a63c111743902fb5ab7ba
Pablo Enrique González Rementeria <pgonzalezr@laberit.com>
2022-07-19 14:47:26 +0200

    finalizado la parte 4 - Agregar servicios

---
+++ src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { MessageService } from './message.service';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add("HeroService: fetched heroes")
+    const heroes = of(HEROES);
+    return heroes;
+  }
+}
