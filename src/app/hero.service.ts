0d2f45138f16b56c3941faca4aad5787a9dccccc
Pablo Enrique González Rementeria <pgonzalezr@laberit.com>
2022-07-20 09:29:06 +0200

    finalizado parte 5 - agregar navegacion en la aplicacion

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,4 +19,9 @@ export class HeroService {
     const heroes = of(HEROES);
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`)
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
