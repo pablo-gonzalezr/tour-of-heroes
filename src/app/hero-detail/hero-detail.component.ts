187849ccd283970c6bc2f13c70e1a3340d538e1c
Pablo Enrique González Rementeria <pgonzalezr@laberit.com>
2022-07-20 12:26:12 +0200

    finalizado parte 6 - obtener datos del servidor

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -6,29 +6,36 @@ import { Location } from '@angular/common';
 
 import { HeroService } from '../hero.service';
 
-
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-
   @Input() hero?: Hero;
 
-  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
   }
 
   getHero(): void {
-    const id = +this.route.snapshot.paramMap.get("id");
-    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
   }
 
   goBack(): void {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => {
+      this.goBack();
+    });
+  }
 }
